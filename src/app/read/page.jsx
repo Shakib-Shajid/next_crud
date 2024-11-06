12090e5cbb35baa8dd1831f484e973c918fc234d
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-07 03:35:34 +0600

    fake data show

--- src/app/read/page.jsx
+++ src/app/read/page.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
     return (
-        <div className='pt-32'>
-            <div className='w-[50%] mx-auto text-center space-y-5 border-2 p-20 rounded-3xl'>
+        <div className='pt-5'>
+            <div className='w-[50%] mx-auto text-center space-y-5 border-2 p-10 rounded-3xl'>
                 <div className="overflow-x-auto">
                     <table className="table text-base">
-                        <caption className='text-xl font-bold text-black p-2 rounded-xl bg-warning mb-5 w-[50%] mx-auto'>Read</caption>
+                        <caption className='text-xl font-bold text-black p-2 rounded-xl bg-warning mb-5 w-[50%] mx-auto'>Read: {users.length}</caption>
                         {/* head */}
                         <thead className='text-white'>
                             <tr>
@@ -18,26 +27,16 @@ const page = () => {
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                            <tr>
-                                <th>1</th>
-                                <td>user1</td>
-                                <td>user1@gmail.com</td>
-                                <td>123456</td>
-                            </tr>
-                            {/* row 2 */}
-                            <tr>
-                                <th>2</th>
-                                <td>user2</td>
-                                <td>user2@gmail.com</td>
-                                <td>654321</td>
-                            </tr>
-                            {/* row 3 */}
-                            <tr>
-                                <th>3</th>
-                                <td>user3</td>
-                                <td>user3@gmail.com</td>
-                                <td>123654</td>
-                            </tr>
+                            {
+                                users.map((user, index) =>
+                                    <tr key={user.id}>
+                                        <td>{index + 1}</td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.address.zipcode}</td>
+                                    </tr>
+                                )
+                            }
                         </tbody>
                     </table>
                 </div>
