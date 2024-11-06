7aa9cfcd16c9c7c1dc7e4cfe61b6bfe46b880141
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-07 02:30:25 +0600

    hover and change headline color slowly

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className="pt-24">
       <div className="text-center rounded-full border-violet-600 border-2 flex flex-col items-center w-[80%] mx-auto p-24">
-        <h3 className="text-3xl font-bold bg-teal-600 text-black inline-block p-5 rounded-3xl w-[40%] mb-20">This is CRUD in Next JS</h3>
+        <h3 className="text-3xl font-bold text-black inline-block p-5 rounded-3xl w-[40%] mb-20 bg-teal-600 
+               bg-hover-slow-transition transition duration-1500">
+          This is CRUD in Next JS
+        </h3>
         <Details />
       </div>
     </div>
