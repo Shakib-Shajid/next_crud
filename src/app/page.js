101dfacf6bafb1c8866e81665524f8dcbecd7f9f
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-16 01:15:27 +0600

    main text width increase

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="pt-24">
       <div className="text-center">
-        <h3 className="text-3xl font-bold text-black inline-block p-5 rounded-3xl md:w-[55%] mb-20 bg-teal-600 
+        <h3 className="text-3xl font-bold text-black inline-block p-5 rounded-3xl md:w-[65%] mb-20 bg-teal-600 
                bg-hover-slow-transition transition duration-1500">
           This is CRUD Operation By Next JS
         </h3>
